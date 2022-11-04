0aec154889552b2d122f27427ac18ee198533a4f
Eng-MAR <Drrahman911@hotmail.com>
2022-11-04 18:13:42 -0400

    Instantiated the radio buttons, EditText, TextView, and Button

--- app/src/main/java/ca/mar/unitconversionapp_midterm/MainActivity.kt
+++ app/src/main/java/ca/mar/unitconversionapp_midterm/MainActivity.kt
@@ -2,10 +2,28 @@ package ca.mar.unitconversionapp_midterm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Instantiating the Radio Buttons, EditText, TextView, and Button
+        val KM = findViewById<RadioButton>(R.id.rbKM)
+        val MK = findViewById<RadioButton>(R.id.rbMK)
+        val IC = findViewById<RadioButton>(R.id.rbIC)
+        val CI = findViewById<RadioButton>(R.id.rbCI)
+        val KG = findViewById<RadioButton>(R.id.rbKG)
+        val GK = findViewById<RadioButton>(R.id.rbGK)
+        val Inp = findViewById<EditText>(R.id.etInp)
+        val Out = findViewById<TextView>(R.id.tvOut)
+        val Con = findViewById<Button>(R.id.button)
+
+
+
     }
 }
